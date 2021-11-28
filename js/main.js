761c520950cf018d7eb757ddbeb70bfdc4dcd8b2
Oleg <79206312+Nayshes@users.noreply.github.com>
2021-11-28 10:30:04 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -30,3 +30,39 @@ window.addEventListener('mousemove', function(e) {
         parallaxAbout.style.transform = 'translate(-' + clientPageWidth * 5 + 'px, -' + clientPageHeight * 15 + 'px)';
     }
 });
+
+
+
+//Прокрутка при клике 
+
+const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+let pageHeight = window.innerHeight;
+if (menuLinks.length > 0) {
+    menuLinks.forEach(menuLink => {
+        menuLink.addEventListener("click", onMenuLinkClick);
+    });
+
+    function onMenuLinkClick(e) {
+        const menuLink = e.target;
+        if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+            const gotoBlock = document.querySelector(menuLink.dataset.goto);
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top;
+
+            if (iconMenu.classList.contains('_active')) {
+                document.body.classList.remove('_lock');
+                iconMenu.classList.remove('_active');
+                MenuBody.classList.remove('_active');
+                MenuBody.classList.remove('menus');
+                mainLogo.classList.remove('_active');
+                preview.classList.remove('_active');
+            }
+
+
+            window.scrollTo({
+                top: gotoBlockValue,
+                behavior: "smooth"
+            });
+            e.preventDefault();
+        }
+    }
+}
